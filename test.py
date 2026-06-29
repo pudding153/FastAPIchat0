532342457f4fed1f3044d3c0e6e55433157251dc
pudding153 <subiriam777@gmail.com>
2026-06-29 16:35:46 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI,HTTPException,status,Request
+from fastapi import FastAPI, HTTPException, status, Request
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
@@ -7,12 +7,14 @@ from google import genai
 from google.genai import types
 from pydantic import BaseModel
 from typing import List
-#key
+
+# key
 load_dotenv()
 key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=key)
 app = FastAPI()
-#通信
+
+# 通信
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -23,29 +25,35 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return{"status":"ok"}
+    return {"status": "ok"}
 
-#受取  #リクエスト
+# 受取  #リクエスト
 @app.post("/api/chat")
 async def chat_endpoint(req: Request):
     body = await req.json()
-    message = body.get("message","")
-#会話履歴
+    message = body.get("message", "")
+    
+    # 会話履歴
     talk = body.get("history", [])
-    talk.append({"role":"user","parts":[{"text":message}]})
-#api設定
-    s = ("setting")
-    ai_config=types.GenerateContentConfig(
+    talk.append({"role": "user", "parts": [{"text": message}]})
+    
+    # api設定
+    s = "setting"
+    ai_config = types.GenerateContentConfig(
         system_instruction=s,
         max_output_tokens=200
-)
+    )
+    
     response = client.models.generate_content(
         contents=talk,
-        model="gemini-3.1-flash-lite",
+        model="gemini-3.1-flash-lite",  
         config=ai_config
-)
- talk.append({"role": "model", "parts": [{"text": response.text}]})
-　return {
+    )
+    
+    talk.append({"role": "model", "parts": [{"text": response.text}]})
+    
+    # 送り返す
+    return {
         "success": True,
         "reply": response.text,
         "history": talk
