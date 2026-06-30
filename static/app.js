fba556014b5f6c8a9007355c37fa74571913142d
pudding153 <subiriam777@gmail.com>
2026-06-30 14:16:40 +0900

    Update app.js

--- static/app.js
+++ static/app.js
@@ -4,4 +4,10 @@ async function send(){
     if (!txt)return;
         log.innerHTML += `<p>自分: ${txt}</p>`;
         input.value = '';
-}
+        const res = await fetch('/api/chat',{
+            method:'POST',
+            headers:{'Content-Type':'application/json'},
+            boby:JSON.stringify({massage:txt,history:history})
+        })
+        const data = await res.json();
+    }
